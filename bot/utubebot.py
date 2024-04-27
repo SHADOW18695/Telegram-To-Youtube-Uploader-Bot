4f5a25e818fcc97623f552edfc7e36c6acb1618b
SHADOW18695 <99009597+SHADOW18695@users.noreply.github.com>
2024-04-27 11:55:54 +0530

    Update utubebot.py

--- bot/utubebot.py
+++ bot/utubebot.py
@@ -22,7 +22,7 @@ from .config import Config
 class UtubeBot(Client):
     def __init__(self):
         super().__init__(
-            session_name="UtubeBot",  # Explicitly providing the name
+            session_name="kamalbabu1bot",  # Explicitly providing the name
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
